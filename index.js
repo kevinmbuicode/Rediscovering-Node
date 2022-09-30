ca741d36ff45d4578257a116214d893b9251a3a5
kevinmbuicode <kevinmushan@gmail.com>
2022-09-30 13:25:52 +0300

    Reading, Writing, Appending files

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-const fs = require('fs');
+const fsPromises = require('fs').promises;
+const path = require('path');
 
-fs.readFile('./files/starter.txt', (err, data) => {
+// const fileOPs = async() => {
+//     try{
+//         const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8');
+//         console.log(data)
+//     } catch (err){
+//         console.log(`There was an error: ${err}`)
+//     }
+// }
+
+// fileOPs();
+
+fs.readFile(path.join(__dirname, 'files', 'starter.txt'), (err, data) => {
     if (err) throw err;
     console.log(data.toString())
 })
+
+
+fs.writeFile(path.join(__dirname, 'files', 'ply.txt'), 'Nice to meet you', (err) => {
+    if (err) throw err;
+    console.log("Write complete")
+})
+
+
+fs.appendFile(path.join(__dirname, 'files', 'ply.txt'), 'Nice to meet you too', (err) => {
+    if (err) throw err;
+    console.log("Append complete")
+})
+
+
 
 //exit on uncaught errors
 process.on('uncaughtException', err => {
