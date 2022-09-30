be0d5f7c3bded2e11d5775c51a000a8dd4c2ad80
kevinmbuicode <kevinmushan@gmail.com>
2022-09-30 23:28:25 +0300

    npm modules

--- index.js
+++ index.js
@@ -1,43 +1,49 @@
-const fsPromises = require('fs').promises;
-const path = require('path');
+// const fsPromises = require('fs').promises;
+// const path = require('path');
 
-const fileOPs = async() => {
-    try{
-        const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8');
-        console.log(data)
-        await fsPromises.writeFile(path.join(__dirname, 'file', 'promiseWrite.txt'), data);
-        await fsPromises.appendFile(path.join(__dirname, 'file', 'promiseWrite.txt'), 'Nice to Meet you.')
-        await fsPromises.rename(path.join(__dirname, 'file', 'promiseWrite.txt'), path.join(__dirname, 'file', 'promiseComplete.txt'))
-        const newData = await fsPromises.readFile(path.join(__dirname, 'files', 'promisesComplete.txt'), 'utf8');
-        console.log(newData)
-    } catch (err){
-        console.log(`There was an error: ${err}`)
-    }
-}
+// const fileOPs = async() => {
+//     try{
+//         const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8');
+//         console.log(data)
+//         await fsPromises.writeFile(path.join(__dirname, 'file', 'promiseWrite.txt'), data);
+//         await fsPromises.appendFile(path.join(__dirname, 'file', 'promiseWrite.txt'), 'Nice to Meet you.')
+//         await fsPromises.rename(path.join(__dirname, 'file', 'promiseWrite.txt'), path.join(__dirname, 'file', 'promiseComplete.txt'))
+//         const newData = await fsPromises.readFile(path.join(__dirname, 'files', 'promisesComplete.txt'), 'utf8');
+//         console.log(newData)
+//     } catch (err){
+//         console.log(`There was an error: ${err}`)
+//     }
+// }
 
-fileOPs();
+// fileOPs();
 
-// fs.readFile(path.join(__dirname, 'files', 'starter.txt'), (err, data) => {
-//     if (err) throw err;
-//     console.log(data.toString())
+// // fs.readFile(path.join(__dirname, 'files', 'starter.txt'), (err, data) => {
+// //     if (err) throw err;
+// //     console.log(data.toString())
+// // })
+
+
+// // fs.writeFile(path.join(__dirname, 'files', 'ply.txt'), 'Nice to meet you', (err) => {
+// //     if (err) throw err;
+// //     console.log("Write complete")
+// // })
+
+
+// // fs.appendFile(path.join(__dirname, 'files', 'ply.txt'), 'Nice to meet you too', (err) => {
+// //     if (err) throw err;
+// //     console.log("Append complete")
+// // })
+
+
+
+// //exit on uncaught errors
+// process.on('uncaughtException', err => {
+//     console.log(`There was an uncaught error: ${err}`)
+//     process.exit(1);
 // })
 
+const { format } = require('date-fns');
+const { v4: uuid} = require('uuid');
 
-// fs.writeFile(path.join(__dirname, 'files', 'ply.txt'), 'Nice to meet you', (err) => {
-//     if (err) throw err;
-//     console.log("Write complete")
-// })
-
-
-// fs.appendFile(path.join(__dirname, 'files', 'ply.txt'), 'Nice to meet you too', (err) => {
-//     if (err) throw err;
-//     console.log("Append complete")
-// })
-
-
-
-//exit on uncaught errors
-process.on('uncaughtException', err => {
-    console.log(`There was an uncaught error: ${err}`)
-    process.exit(1);
-})
+console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'))
+console.log(uuid())
