0072e96c556bfa6932b759f6e54fb8496899475f
kevinmbuicode <kevinmushan@gmail.com>
2022-10-03 15:58:02 +0300

    renamed data folder to employees

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const data = {};
-data.employees = require('../../data/employees.json');
+data.employees = require('../../model/employees.json');
 
 router.route('/')
     .get((req, res) => {
